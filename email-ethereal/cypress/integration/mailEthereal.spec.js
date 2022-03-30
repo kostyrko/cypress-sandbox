14e3a2e87464a50d83318cd1f5adbaea00304238
Mikołaj Kostyrko <mkostyrko@gmail.com>
2022-03-30 14:13:19 +0200

    Update ethereal email test

---
+++ email-ethereal/cypress/integration/mailEthereal.spec.js
@@ -0,0 +1,55 @@
+/// <reference types="cypress" />
+// https://github.com/bahmutov/cypress-recurse
+const { recurse } = require('cypress-recurse')
+
+describe('Email confirmation', () => {
+    let userEmail
+
+    before(() => {
+        // get and check the test email only once before the tests
+        cy.task('getUserEmail').then((email) => {
+            expect(email).to.be.a('string')
+            userEmail = email
+        })
+    })
+
+    it('Registers a user on Twitter', () => {
+        const userName = 'FirstName LastName'
+        cy.visit('localhost:3000')
+        cy.get('#name').type(userName)
+        cy.get('#email').type(userEmail)
+        cy.get('#company_size').select('3')
+        cy.get('button[type=submit]').click()
+        cy.log('**shows message to check emails**')
+        cy.get('[data-cy=sent-email-to]')
+        .should('be.visible')
+        .and('have.text', userEmail)
+        // cy.wait(5000)
+        recurse(
+            () => cy.task('getLastEmail'), // Cypress commands to retry
+            Cypress._.isObject, // keep retrying until the task returns an object
+            {
+              timeout: 60000, // retry up to 1 minute
+              delay: 5000, // wait 5 seconds between attempts
+            },
+          )
+        .then((email) => {
+            cy.document({ log: false }).invoke({ log: false }, 'write', email.html)
+        })
+        cy.log('**email has the confirmation code**')
+    cy.get('[data-cy=confirmation-code]')
+      .should('be.visible')
+      .invoke('text')
+      .then((code) => {
+        expect(code, 'confirmation code')
+          .to.be.a('string')
+          .and.have.length.gt(5)
+        cy.contains('Confirm registration').click()
+        cy.get('#confirmation_code', { timeout: 10000 }).type(code)
+        cy.get('button[type=submit]').click()
+        cy.get('[data-cy=confirmed-code]').should('be.visible')
+        cy.get('[data-cy=incorrect-code]').should('not.exist')
+      })
+    })
+})
+
